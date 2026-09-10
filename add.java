b8d2d27b1d335ec8d0a6643407e744086e57a9f7
demo <gokulnila2122004@gmail.com>
2026-09-10 22:06:12 +0530

    1st verstion

---
+++ add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class add 
+{
+    public static void main (String []args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("The 1st number is :");
+        int a = s.nextInt();
+        System.out.println("The 2nd number is :");
+        int b = s.nextInt();
+        int c = a+b;
+        System.out.println(" The added number is :"+c);
+    }
+}
