2bbb5a0958c1e1655cdb172911b2be30f55248ef
Gokulnila <gokulnila2122004@gmail.com>
2026-09-11 21:20:10 +0530

    Implement basic ATM withdrawal functionality

--- Atm.java
+++ Atm.java
@@ -1,3 +1,48 @@
-public class Atm {
+import java.io.*;
+import java.util.*;
+ 
+
+class Atm
+{
+  public static void main (String []args)
+  
+ {
+
+
+  Scanner s =  new Scanner(System.in);
+
+  int  balance =  10000;
+  String  select = "";
+ 
+
+ while(!select.equals("No"))
+ {
+  
+   System.out.println("Enter the widthdrawel amount : ");
+   int amount =  s.nextInt();
+ 
+    if (amount<=balance)
+      {
+      System.out.println("Withdrawel Sucessfull");
+      balance =  balance- amount ;
+      System.out.println("Remaining Amount is :" +balance);
+
+      }
+     else
+      {
+      
+      System.out.println("Insufficent balance");
+
+       }
     
+      System.out.println(" Do you want to continue: ") ; 
+      select = s.next();
+
+  }
+
+  
+      System.out.println(" Thank you for using the Atm:") ; 
+
+ 
+}
 }
